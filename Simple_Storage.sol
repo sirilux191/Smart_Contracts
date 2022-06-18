1d37cc84941018941f8c469a3ec9d4836298928e
Aaditya Chandankar <aadityachandankar@gmail.com>
2022-06-18 11:01:34 +0530

    Simple Storage Tutorial done through Freecodecamp

---
+++ Simple_Storage.sol
@@ -0,0 +1,41 @@
+
+//SPDX-License-Identifier: MIT
+pragma solidity ^0.6.0;  //Defining Version of Solidity
+
+
+contract SimpleStorage {                    //Contract Intiation
+
+    uint256 favoriteNumber;                 //Declaring varible type and varible
+    
+    //this will get intialized to 0 by default!
+
+    struct People {                         //Declaring Struct People to have favoriteNumber & name assoicated
+        uint256 favoriteNumber; 
+        string name;
+    }
+
+    People[] public people;                 //Declaring People array, so multiple number-name pair can be stored
+
+    mapping(string => uint256)public nameToFavoriteNumber;         //mapping to find favorite number of people by name
+
+    /*Store Function to store favoriteNumber, 
+    public Visibility Quantifier so it can accessible to anyone,*/
+    
+    function store (uint256 _favoriteNumber) public {           
+        favoriteNumber =_favoriteNumber;
+
+    }
+    
+    // To retrieve the favorite Number Stored, Only recent favoriteNumber
+
+    function retrieve() public view returns(uint256) {          
+        return favoriteNumber;    }
+    
+    //addPerson Function to store favoriteNumber-name pair
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        people.push(People(_favoriteNumber,_name));                             //Pushing favoriteNumber & Name to People array
+        nameToFavoriteNumber[_name] = _favoriteNumber;                          //Mapping of favoriteNumber & Name
+    }
+
+}
